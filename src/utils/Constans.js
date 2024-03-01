035de95b160199e7757059c27d1d3e7407efa569
Michael Diaz <michaeldiazcampo@gmail.com>
2024-02-29 19:42:34 -0500

    first commit

---
+++ src/utils/Constans.js
@@ -0,0 +1,26 @@
+export const menu = [
+  {
+    id: 0,
+    section: "Home",
+  },
+  {
+    id: 1,
+    section: "About Us",
+  },
+  {
+    id: 2,
+    section: "Services",
+  },
+  {
+    id: 3,
+    section: "Portfolio",
+  },
+  {
+    id: 4,
+    section: "Pages",
+  },
+  {
+    id: 5,
+    section: "Contact Us",
+  },
+];
